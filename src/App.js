1896db3a99c7551f06dcc983768f8cbc1addbb97
pistachionet <mynavid@verizon.net>
2023-04-08 17:05:17 -0400

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import React, { useState } from 'react'; // useState is a hook
 import logo from './logo.svg';
 import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login'); // useState hook
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        /*ternary operator check if login works, if not, then register*/
+        currentForm === 'login' ? <Login/> : <Register/> // 
+      }
+       
     </div>
   );
 }
